f3680f60b812aef34f44e8c2a0afd39ac7f20f6a
miguel76 <miguel.ceriani@gmail.com>
2016-09-21 17:39:27 +0200

    Log client events with bunyan

--- io.js
+++ io.js
@@ -1,11 +1,13 @@
-var socket = require('socket.io');
+const socket = require('socket.io');
+const bunyan = require('bunyan');
 
+var log = bunyan.createLogger({name: "client-events"});
 module.exports = function (httpServer) {
   var io = socket(httpServer);
   io.on('connection', function(socket) {
     var traceEvent = function(event) {
       event.sessionId = socket.id;
-      console.log(event);
+      log.info({event: event});
     };
     traceEvent({type: 'connection'});
     ['load-snapshot', 'save-snapshot', 'create', 'delete', 'move', 'ui'].map(function(eventType) {
