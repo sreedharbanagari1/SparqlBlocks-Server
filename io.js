2f35059e126441daa529c3b51dea36381fb0fefb
miguel76 <miguel.ceriani@gmail.com>
2016-09-21 18:32:43 +0200

    Sending logs to loggly through winston

--- io.js
+++ io.js
@@ -1,13 +1,21 @@
 const socket = require('socket.io');
-const bunyan = require('bunyan');
+const winston = require('winston');
+require('winston-loggly-bulk');
 
-var log = bunyan.createLogger({name: "client-events"});
+winston.add(winston.transports.Loggly, {
+  token: "ee08bcff-7d69-4b60-8c88-edc872f26116",
+  subdomain: "sparqlblocks",
+  tags: ["Winston-NodeJS"],
+  json:true
+});
+
+// var log = bunyan.createLogger({name: "client-events"});
 module.exports = function (httpServer) {
   var io = socket(httpServer);
   io.on('connection', function(socket) {
     var traceEvent = function(event) {
       event.sessionId = socket.id;
-      log.info({event: event});
+      winston.log('info', 'client-event', {event: event});
     };
     traceEvent({type: 'connection'});
     ['load-snapshot', 'save-snapshot', 'create', 'delete', 'move', 'ui'].map(function(eventType) {
