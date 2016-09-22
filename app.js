b6a24ae4aded6c8121bc13c41f66c5b1ad25f0ba
miguel76 <miguel.ceriani@gmail.com>
2016-09-22 18:13:08 +0200

    Clean Up

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var express = require('express'),
 
 var app = express();
 
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
     '/demo',
