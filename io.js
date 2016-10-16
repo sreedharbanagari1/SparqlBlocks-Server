c0568e440d764e7869ec75fd617a30dd66324a3f
miguel76 <miguel.ceriani@gmail.com>
2016-10-16 20:19:22 +0200

    Add Option to Log to AWS CloudWatch

--- io.js
+++ io.js
@@ -1,6 +1,7 @@
 const socket = require('socket.io');
 const winston = require('winston');
 require('winston-loggly-bulk');
+const winstonAwsCloudWatch = require('winston-aws-cloudwatch');
 
 if (process.env.LOGGLY_SUBDOMAIN && process.env.LOGGLY_TOKEN) {
   winston.add(winston.transports.Loggly, {
@@ -8,6 +9,18 @@ if (process.env.LOGGLY_SUBDOMAIN && process.env.LOGGLY_TOKEN) {
     subdomain: process.env.LOGGLY_SUBDOMAIN,
     tags: process.env.LOGGLY_TAG ? [process.env.LOGGLY_TAG] : [],
     json:true
+  });
+}
+
+if (process.env.AWS_CW_LOG_GROUP_NAME && process.env.AWS_CW_LOG_STREAM_NAME) {
+  winston.add(winstonAwsCloudWatch, {
+    logGroupName: process.env.AWS_CW_LOG_GROUP_NAME,
+    logStreamName: process.env.AWS_CW_LOG_STREAM_NAME,
+    awsConfig: {
+      accessKeyId: process.env.AWS_CFG_KEY_ID,
+      secretAccessKey: process.env.AWS_CFG_ACCESS_KEY,
+      region: process.env.AWS_CFG_REGION
+    }
   });
 }
 
