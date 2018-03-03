6fbc524c23421612f2e897092e7b8b34c312a700
sreedharbanagari1 <37022660+sreedharbanagari1@users.noreply.github.com>
2018-03-03 18:44:36 +0100

    receiving request from SparqlBlocks.
    i.e when the request comes from sparqlblocks about generating HTML+SVG page, this app.js file receives a request and send those request to Routes in order to generate HTML+SVG page with export of query and results. and from the same page we have lot request sent to app.js file. i.e in order to download a  query (textual and visual) and results in JSON file and one more request for History of same query with differet results.

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 var express = require('express'),
     fs = require('fs'),
     path = require('path');
-
+//var query = require('./routes/query');  // request to query.js file
+var query = require('./routes/query');    // request to query1.js file
+var gist = require('./routes/gist');
+var gist1 = require('./routes/gist1');
+var query1 = require('./routes/query1');
+var json = require('./routes/json1');     // request to json1.js file
+var svg = require('./routes/svg1');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 var app = express();
+// view engine setup
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
     '/demo',
     express.static(path.join(__dirname, 'node_modules', 'sparqlblocks', 'dist')));
 
+app.use('/query',query);
+app.use('/gist',gist);
+app.use('/gist1',gist1);
+app.use('/query1',query1);     // request comes from query.ejs file for downloading the sparqlquery
+app.use('/json1',json);     //request comes from query.ejs file for downloading the Results
+app.use('/svg1',svg);
 module.exports = app;
